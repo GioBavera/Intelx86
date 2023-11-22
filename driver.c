85b81d450c2a85dea37a832ca476d75de67cab03
GioBavera <149208089+GioBavera@users.noreply.github.com>
2023-11-22 14:51:01 -0300

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 //Prototipo de la funcion det
-int det(int, int, int, int) __attribute((cdecl))__;
+int det(int a, int b, int c, int d) __attribute((cdecl))__;
 
 int main(void){
-	int a=1, b=2, c=3, d=4;
-	int resultado = det(a,b,c,d);
+	int resultado, a, b, c, d;
+	printf("Ingrese sus numeros: ");
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	scanf("%d", &d);
+	resultado = det(a,b,c,d);
 	printf("El resultado es: %d \n", resultado);
 	return 0;
 }
