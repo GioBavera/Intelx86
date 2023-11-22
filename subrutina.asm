4df2e9497860001e461106e0f02206f1bc496d64
GioBavera <149208089+GioBavera@users.noreply.github.com>
2023-11-22 14:59:55 -0300

    Update subrutina.asm

--- subrutina.asm
+++ subrutina.asm
@@ -4,12 +4,18 @@
 segment .text
  global det
 
-det:	mov eax, edi
-	imul ecx
-	mov ebx, eax
+det:	enter 16, 0        ; Espacio en el stack para los tres argumentos
+	push ebx           ; Preservo el registro
 
-	mov eax, esi
-	imul edx
-	
-	sub eax, ebx
+	mov ecx, [ebp+8]   ; Guarda el valor de a en ecx
+	imul ecx, [ebp+16] ; Hago: a.c
+
+	mov edx, [ebp+12]  ; Guarda el valor de b en edx
+	imul edx, [ebp+20] ; Hago: b.d
+
+	sub ecx, edx       ; Hago: a.c - b.d
+	mov eax, ecx       ; Muevo el resultado a eax
+
+	pop ebx
+	leave
 	ret
